54911dd1e27146abd46235ecc1de79913a47394d
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2025-01-08 19:25:39 -0300

    Done Home

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "./style.css";
 import { greeting } from "./greeting.js";
 
+const githubButton = document.querySelector('.github');
+const linkedinButton = document.querySelector('.linkedin');
+const navSections = document.querySelectorAll('.nav-section');
+
+navSections.forEach((section) => {
+    section.addEventListener('click', () => {
+        navSections.forEach((s) => s.classList.remove('active'));
+        section.classList.add('active');
+    });
+});
+
+githubButton.addEventListener('click', () => {
+    window.open("https://github.com/valdemarvictorleitecarvalho", "_blank");
+});
+
+linkedinButton.addEventListener('click', () => {
+    window.open("https://www.linkedin.com/in/valdemar-carvalho-815408334/", "_blank"); 
+});
+
 console.log(greeting);
