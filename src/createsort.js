50832fb4cf114793a376e353a14c52793b8ca001
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2025-01-10 17:04:32 -0300

    Add Dialog Inputs

--- src/createsort.js
+++ src/createsort.js
@@ -6,3 +6,33 @@ export function showDialogCreateSort() {
         dialog.style.display = 'flex';
     });
 }
+
+export function createInputs() {
+    const selectElement = document.getElementById("number-select");
+    const inputContainer = document.getElementById("input-container");
+    
+    inputContainer.innerHTML = '';
+
+    const quantity = parseInt(selectElement.value, 10);
+
+    for (let i = 0; i < quantity; i++) {
+        const divLabel = document.createElement("div-label");
+        
+        const label = document.createElement("label");
+        label.setAttribute("for", `input-number-${i}`);
+        label.textContent = `Digite o número ${i + 1}:`;
+
+        const input = document.createElement("input");
+        input.type = "text";
+        input.id = `input-number-${i}`;
+        input.name = `inputNumber-${i}`;
+        input.required = true;
+
+
+        divLabel.appendChild(label);
+        divLabel.appendChild(input);
+        
+        inputContainer.appendChild(divLabel);
+    }
+    selectElement.addEventListener("change", createInputs);
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { initSocialLinks } from "./myinfo.js";
 import { activeCodeHighlight } from "./codehigh.js";
 import { trackHeadingClick } from "./htracker.js";
 import { showDialogCreateSort } from "./createsort.js";
+import { createInputs } from "./createsort.js";
+
+createInputs();
 
 showDialogCreateSort();
 
